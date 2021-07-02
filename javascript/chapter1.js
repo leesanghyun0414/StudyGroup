1336b0e06176207943a1637ffe22b69b226598a3
sonoda0529 <kojurinnmymelo@gmail.com>
2021-07-02 22:41:24 +0900

    vouge japan

--- javascript/chapter1.js
+++ javascript/chapter1.js
@@ -1,2 +1,35 @@
 
+let todo = ['デザインカンプ作成', 'データ整理','勉強会申し込み','牛乳買う'];
+todo.push('歯医者に行く');
+for(let item of todo){
+  const li = `<li>${item}</li>`;
+document.getElementById('list').insertAdjacentHTML('beforeend',li);
+}
+
+
+
+
+
+.country-name2 {margin-top: 22px;}
+
+
+margin-top: 1px;
+list-style: none;
+justify-content: flex-end;
+padding-right: 10px;
+text-transform: uppercase;
+
+padding-top: 150px;
+
+
+
+.icon {display: flex;
+  height: 20%;
+
+}
+.second-title {
+  height: 40%;
+
+}
+
 
